f09815a90cb72d036f47bc79fb2275cc7961a856
T-pl <tiagofqu@gmail.com>
2022-07-19 15:52:24 -0300

    VIP-005: Começando a integrar o front com o back

--- src/components/Dataitem/Dataitem.jsx
+++ src/components/Dataitem/Dataitem.jsx
@@ -1,12 +1,12 @@
 import './dataitem.css'
 
-export function Dataitem() {
+export function Dataitem(props) {
   return (
     <div className="dataProd">
-      <span className='titleProd'>Câmera interna inteligente Wi-Fi Full </span>
-      <span className='marca'>Intelbras</span>
+      <span id={props.id} className='titleProd'>Câmera interna inteligente Wi-Fi Full </span>
+      <span id={props.name} className='marca'>Intelbras</span>
       <div className="valores">
-        <span>R&#36;: </span>
+        <span >R&#36;: </span>
         <span >300&#44;00  </span>
       </div>
       <span className='color'>Cor: Branco</span>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -12,19 +12,20 @@ export function Products() {
     alert('Tem certeza que deseja excluir o Produto?')
   }
   return (
-    <div className="products">
-      <div className="infoProducts">
-        <img src={imgPro} alt="" />
-        <Dataitem />
+    <li>
+      <div className="products">
+        <div className="infoProducts">
+          <img src={imgPro} alt="" />
+          <Dataitem />
+        </div>
+        <div className="crudProd">
+          <ul className='actionList'>
+            <li > <Link exact to="/carrinho"><img className="iconCar" src={carrinho} alt="Carrinho de Compras" /></Link> </li>
+            <li> <Link exact to="/editar"><img className="iconEdit" src={edit} alt="Icone Editar" /> </Link> </li>
+            <li> <button className='btnDelete' onClick={checkDelet}> <img src={delet} alt="Icone Deletar" className='iconDelete' /></button> </li>
+          </ul>
+        </div>
       </div>
-      <div className="crudProd">
-        <ul className='actionList'>
-          <li > <Link exact to="/carrinho"><img className="iconCar" src={carrinho} alt="Carrinho de Compras" /></Link> </li>
-          <li> <Link exact to="/editar"><img className="iconEdit" src={edit} alt="Icone Editar" /> </Link> </li>
-          <li> <button className='btnDelete' onClick={checkDelet}> <img src={delet} alt="Icone Deletar" className='iconDelete' /></button> </li>
-        </ul>
-      </div>
-
-    </div>
+    </li>
   );
 }
